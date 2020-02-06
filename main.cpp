51af79f87cf9215a1e536a1887fccf710b515d06
oushu1zhangdezhu2 <zhang.dezhu@oushu.io>
2020-02-06 16:48:18 +0800

    zzz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"Hello Git"<<endl;
-    return 0;
-    }
+int main (int argc, char** argv)
+   {
+       int* array = new int[100];
+       int res = array[100];
+       delete [] array;
+       return res;
+  } 
